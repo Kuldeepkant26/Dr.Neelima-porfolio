4eb49fe61ee5bbf780ebb97f2f06bb1c9e46d390
Kuldeepkant26 <picasuplayers@gmail.com>
2024-04-20 07:26:37 +0530

    hi

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
+// NAVBAR
+function toggleMenu() {
+    document.querySelector('.menu').classList.toggle('show');
+}
+//HOME PAGE SECTION 6 STARTS
+let rdm = document.querySelector('.readmore');
+let rdm2 = document.querySelector('.readmore2');
+let mc = document.querySelector('.morecontent');
+let mc2 = document.querySelector('.morecontentx');
+
+
+rdm.addEventListener("click", () => {
+
+    mc.classList.toggle("morecontent2");
+
+});
+rdm2.addEventListener("click", () => {
+
+    mc2.classList.toggle("morecontent2");
+
+});
+
+//HOME PAGE SECTION 6 ENDS
+
+
+
+//HOME PAGE FOCOUS AREA SECTION STARTS
+
+
 let red = document.querySelector("#red");
 let blue = document.querySelector("#blue");
 let reds = document.querySelectorAll(".reds");
 let blues = document.querySelectorAll(".blues");
-// red.addEventListener("click", () => {
-//     this.style.backgroundColor = "red";
-//     document.querySelector("#blue").style.backgroundColor = "none";
-//     for (r of reds) {
-//         r.style.display = "block";
-//     }
-//     for (b of blues) {
-//         b.style.display = "none";
-//     }
-// });
-// blue.addEventListener("click", () => {
-//     this.style.backgroundColor = "blue";
-//     document.querySelector("#red").style.backgroundColor = "none";
-//     for (r of reds) {
-//         r.style.display = "none";
-//     }
-//     for (b of blues) {
-//         b.style.display = "block";
-//     }
-// });
 
 red.addEventListener("click", (event) => {
     event.target.style.textDecoration = "underline";
@@ -44,6 +53,8 @@ blue.addEventListener("click", (event) => {
         b.style.display = "block";
     }
 });
+
+//HOME PAGE FOCOUS AREA SECTION ENDS
 
 
 
